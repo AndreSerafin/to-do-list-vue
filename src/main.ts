751015848a8600edd3af8c38a78dc788629f4212
Andre Pereira <andreserafim11@gmail.com>
2024-05-09 13:09:49 -0300

    feat: comments

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
+// Importando a função createApp do pacote 'vue'
 import { createApp } from 'vue'
+
+// Importando o arquivo CSS global
 import './global.css'
+
+// Importando o componente principal da aplicação
 import App from './App.vue'
+
+// Importando os ícones Phosphor
 import PhosphorIcons from '@phosphor-icons/vue'
 
+// Criando uma instância do aplicativo Vue
 let app = createApp(App)
 
+// Utilizando o plugin PhosphorIcons na instância do aplicativo Vue
 app.use(PhosphorIcons)
 
+// Montando o aplicativo Vue na div com o id '#app' no documento HTML
 app.mount('#app')
