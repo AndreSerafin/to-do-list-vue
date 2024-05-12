1911eac7d8f149ea17fec63e7749e3dfb01c6db7
Andre Pereira <andreserafim11@gmail.com>
2024-05-12 13:53:30 -0300

    feat: pratica 01

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,16 @@ import App from './App.vue'
 // Importando os ícones Phosphor
 import PhosphorIcons from '@phosphor-icons/vue'
 
+// Importando router do vue router 
+import router from './router/index.ts'
+
 // Criando uma instância do aplicativo Vue
 let app = createApp(App)
 
 // Utilizando o plugin PhosphorIcons na instância do aplicativo Vue
 app.use(PhosphorIcons)
+app.use(router)
+
 
 // Montando o aplicativo Vue na div com o id '#app' no documento HTML
 app.mount('#app')
